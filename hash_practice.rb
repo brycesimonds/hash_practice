8057567e055ac8273fd0fc39706479bb6868128c
Bryce Simonds <bsimonds14@gmail.com>
2022-04-21 19:02:09 -0600

    Intial commit

---
+++ hash_practice.rb
@@ -0,0 +1,14 @@
+person = {
+  "name" => "George",
+  "age" => "30",
+  "location" => "Denver",
+  "married" => false
+}
+puts person
+
+puts person.first
+# This will return name on one line, and George
+# on a line below it.
+# We were expecting this to look like:
+# "name" => "George"
+# We are both going to work on this :) (>.<) 
